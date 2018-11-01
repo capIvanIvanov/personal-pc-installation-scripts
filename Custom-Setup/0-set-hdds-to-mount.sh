1e8207d5bcd578654b16df44c0f94e16c8899073
Ivan Ivanov (Main PC) <ivan.ivanov.cigsso@gmail.com>
2018-11-01 10:18:50 +0200

    Finished the NFS setup script.

--- Custom-Setup/0-set-hdds-to-mount.sh
+++ Custom-Setup/0-set-hdds-to-mount.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir /home/ivan/Virtual-Machines
+mkdir /home/ivan/Virtual-Machines-2
 mkdir /home/ivan/Storage
 mkdir /home/ivan/Install-Sets
 mkdir /home/ivan/Movies
@@ -8,11 +9,14 @@ mkdir /home/ivan/Movies/Documentary
 mkdir /home/ivan/Movies/Anime
 mkdir /home/ivan/Movies/Movies
 
-sudo su -c "echo '# Mount /dev/sdb4 on /home/ivan/Documents...' >> /etc/fstab"
-sudo su -c "echo 'UUID=1248C54C3475BE20                     /home/ivan/Documents		ntfs	defaults	0	2' >> /etc/fstab"
-
 sudo su -c "echo '# Mount /dev/sdc7 on /home/ivan/Virtual-Machines...' >> /etc/fstab"
 sudo su -c "echo 'UUID=b10e583a-0cda-4749-9fc6-d78276b18d02 /home/ivan/Virtual-Machines	ext4	defaults	0	2' >> /etc/fstab"
+
+sudo su -c "echo '# Mount /dev/sdd1 on /home/ivan/Documents...' >> /etc/fstab"
+sudo su -c "echo 'UUID=1248C54C3475BE20                     /home/ivan/Documents		ntfs	defaults	0	2' >> /etc/fstab"
+
+sudo su -c "echo '# Mount /dev/sde4 on /home/ivan/Virtual-Machines-2...' >> /etc/fstab"
+sudo su -c "echo 'UUID=fee0d053-a515-4177-b379-59a6bda9fc0c /home/ivan/Virtual-Machines-2	ext4	defaults	0	2' >> /etc/fstab"
 
 sudo su -c "echo '# Mount /dev/sdf1 on /home/ivan/Storage...' >> /etc/fstab"
 sudo su -c "echo 'UUID=AC0209D60209A684                     /home/ivan/Storage		ntfs	defaults	0	2' >> /etc/fstab"
@@ -23,13 +27,14 @@ sudo su -c "echo 'UUID=CC3C17DE3C17C27E                     /home/ivan/Install-S
 sudo su -c "echo '# Mount /dev/sdh1 on /home/ivan/Videos...' >> /etc/fstab"
 sudo su -c "echo 'UUID=A232AB3732AB0F7B                     /home/ivan/Videos		ntfs	defaults	0	2' >> /etc/fstab"
 
-sudo su -c "echo '# Mount /dev/sd on /home/ivan/Movies/Documentary...' >> /etc/fstab"
+sudo su -c "echo '# Mount /dev/sdh2 on /home/ivan/Movies/Documentary...' >> /etc/fstab"
 sudo su -c "echo 'UUID=B626C19726C158CF                     /home/ivan/Movies/Documentary	ntfs	defaults	0	2' >> /etc/fstab"
 
-sudo su -c "echo '# Mount /dev/sd on /home/ivan/Movies/Anime...' >> /etc/fstab"
+sudo su -c "echo '# Mount /dev/sdh3 on /home/ivan/Movies/Anime...' >> /etc/fstab"
 sudo su -c "echo 'UUID=2AB2E3C6B2E3949B                     /home/ivan/Movies/Anime	ntfs	defaults	0	2' >> /etc/fstab"
 
-sudo su -c "echo '# Mount /dev/sd on /home/ivan/Movies/Movies...' >> /etc/fstab"
+sudo su -c "echo '# Mount /dev/sdh4 on /home/ivan/Movies/Movies...' >> /etc/fstab"
 sudo su -c "echo 'UUID=1016F1F416F1DB24                     /home/ivan/Movies/Movies	ntfs	defaults	0	2' >> /etc/fstab"
 
 sudo su -c "chown --recursive ivan:ivan /home/ivan/Virtual-Machines"
+sudo su -c "chown --recursive ivan:ivan /home/ivan/Virtual-Machines-2"
--- Custom-Setup/9-setup-NFS-sharing.sh
+++ Custom-Setup/9-setup-NFS-sharing.sh
@@ -3,6 +3,21 @@
 sudo apt-get update
 sudo apt-get -y install nfs-kernel-server nfs-common
 
-sudo su -c "echo '/home/ivan/Documents 192.168.1.146(ro,sync,wdelay)' >> /etc/exports"
-sudo su -c "echo '/home/ivan/Storage 192.168.1.146(ro,sync,wdelay)' >> /etc/exports"
-sudo su -c "echo '/home/ivan/Install-Sets 192.168.1.146(ro,sync,wdelay)' >> /etc/exports"
+#sudo su -c "echo '/home/ivan/Documents 192.168.1.143(ro,sync,wdelay)' >> /etc/exports"
+#sudo su -c "echo '/home/ivan/Storage 192.168.1.143(ro,sync,wdelay)' >> /etc/exports"
+#sudo su -c "echo '/home/ivan/Install-Sets 192.168.1.143(ro,sync,wdelay)' >> /etc/exports"
+
+mkdir /home/ivan/Server
+mkdir /home/ivan/Server/Documents
+mkdir /home/ivan/Server/Storage
+mkdir /home/ivan/Server/Storage-2
+
+
+sudo su -c "echo '#Mount Packard-Bell.s Documents on ~/Server/Documents' >> /etc/fstab"
+sudo su -c "echo '192.168.1.146:/home/ivan/Documents        /home/ivan/Server/Documents	nfs	defaults	0	2' >> /etc/fstab"
+
+sudo su -c "echo '#Mount Packard-Bell.s Storage on ~/Server/Storage' >> /etc/fstab"
+sudo su -c "echo '192.168.1.146:/home/ivan/Storage          /home/ivan/Server/Storage	nfs	defaults	0	2' >> /etc/fstab"
+
+sudo su -c "echo '#Mount Packard-Bell.s Storage-2 on ~/Server/Storage-2' >> /etc/fstab"
+sudo su -c "echo '192.168.1.146:/home/ivan/Storage-2        /home/ivan/Server/Storage-2	nfs	defaults	0	2' >> /etc/fstab"

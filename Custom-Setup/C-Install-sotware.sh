1fbe88503c4b4591c53a8de4cfbc5b4fac5ac9e0
Ivan Ivanov (Main PC) <ivan.ivanov.cigsso@gmail.com>
2018-11-03 18:36:33 +0200

    Updated C-Install-sotware.sh and setup-kvm.sh

--- Custom-Setup/C-Install-sotware.sh
+++ Custom-Setup/C-Install-sotware.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+read -p "Press <ENTER> to start the script or stop it and check for newer version of AnyDesk"
+
 # Install VMware player...
 read -p "Press <ENTER> to install VMWare Player"
 sudo apt-get -y install gcc-8 g++-8
+cd ~/Downloads
 wget -c https://www.vmware.com/go/getplayer-linux
 chmod u+x getplayer-linux
 sudo ./getplayer-linux
 
+read -p "Press <ENTER> to install AnyDesk (Check for newer version)"
+cd ~/Downloads
+wget https://download.anydesk.com/linux/anydesk_4.0.0-1_amd64.deb
+sudo dpkg -i anydesk_4.0.0-1_amd64.deb
+
+read -p "Press <ENTER> to install Viber"
+cd ~/Downloads
+wget -O ./viber.deb http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+dpkg-deb -x viber.deb viber
+dpkg-deb --control viber.deb viber/DEBIAN
+sed -i 's/libcurl3/libcurl4/' viber/DEBIAN/control 
+dpkg -b viber vibernew.deb
+sudo dpkg -i vibernew.deb
+
 read -p "Press <ENTER> to install foobar2000"
-cd /home/ivan/Install-Sets/Programs/foobar2000
+cd ~/Install-Sets/Programs/foobar2000
 wine foobar2000_v1.4.exe
-
-read -p "Press <ENTER> to continue"
-
-#cd ...
--- Custom-Setup/setup-kvm.sh
+++ Custom-Setup/setup-kvm.sh
@@ -3,14 +3,14 @@
 sudo apt-get -y install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager
 sudo apt-get -y install net-tools
 
-sudo su -c "echo '# interfaces(5) file used by ifup(8) and ifdown(8)' >  /etc/network/interfaces" # On purpose! We want to errase the whole file!
-sudo su -c "echo 'auto lo br0'                                        >> /etc/network/interfaces"
-sudo su -c "echo 'iface lo inet loopback'                             >> /etc/network/interfaces"
-sudo su -c "echo '	'                                                 >> /etc/network/interfaces"
-sudo su -c "echo 'iface eno1 inet manual'                             >> /etc/network/interfaces"
-sudo su -c "echo '	'                                                 >> /etc/network/interfaces"
-sudo su -c "echo 'iface br0 inet dhcp'                                >> /etc/network/interfaces"
-sudo su -c "echo '	bridge_ports eno1'                                >> /etc/network/interfaces"
+#sudo su -c "echo '# interfaces(5) file used by ifup(8) and ifdown(8)' >  /etc/network/interfaces" # On purpose! We want to erase the whole file!
+#sudo su -c "echo 'auto lo br0'                                        >> /etc/network/interfaces"
+#sudo su -c "echo 'iface lo inet loopback'                             >> /etc/network/interfaces"
+#sudo su -c "echo '	'                                                 >> /etc/network/interfaces"
+#sudo su -c "echo 'iface eno1 inet manual'                             >> /etc/network/interfaces"
+#sudo su -c "echo '	'                                                 >> /etc/network/interfaces"
+#sudo su -c "echo 'iface br0 inet dhcp'                                >> /etc/network/interfaces"
+#sudo su -c "echo '	bridge_ports eno1'                                >> /etc/network/interfaces"
 
 sudo adduser ivan libvirt
 sudo adduser ivan libvirt-qemu

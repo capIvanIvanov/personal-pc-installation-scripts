7870044f7daa36ccbea21bfa354a04bb1f76c8a3
Ivan Ivanov (Main PC) <ivan.ivanov.cigsso@gmail.com>
2018-11-04 09:53:03 +0200

    Completed C-Install-sotware.sh

--- Custom-Setup/C-Install-sotware.sh
+++ Custom-Setup/C-Install-sotware.sh
@@ -1,29 +1,59 @@
 #!/bin/bash
 
-read -p "Press <ENTER> to start the script or stop it and check for newer version of AnyDesk"
+read -n1 -rsp "Preass <c> to start the script or <any other key> to quit and check for newer version of AnyDesk" result
 
-# Install VMware player...
-read -p "Press <ENTER> to install VMWare Player"
-sudo apt-get -y install gcc-8 g++-8
-cd ~/Downloads
-wget -c https://www.vmware.com/go/getplayer-linux
-chmod u+x getplayer-linux
-sudo ./getplayer-linux
+if [ $result = 'c' ]; then
+	printf "\nContunuing...\n"
+else
+	printf "\nExiting...\n"
+	exit 0
+fi
 
-read -p "Press <ENTER> to install AnyDesk (Check for newer version)"
-cd ~/Downloads
-wget https://download.anydesk.com/linux/anydesk_4.0.0-1_amd64.deb
-sudo dpkg -i anydesk_4.0.0-1_amd64.deb
+read -n1 -rsp "Preass <c> to install VMware Player or <any other key> to skip" result
 
-read -p "Press <ENTER> to install Viber"
-cd ~/Downloads
-wget -O ./viber.deb http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
-dpkg-deb -x viber.deb viber
-dpkg-deb --control viber.deb viber/DEBIAN
-sed -i 's/libcurl3/libcurl4/' viber/DEBIAN/control 
-dpkg -b viber vibernew.deb
-sudo dpkg -i vibernew.deb
+if [ $result = 'c' ]; then
+	printf "\nInstalling...\n"
+	sudo apt-get -y install gcc-8 g++-8
+	cd ~/Downloads
+	wget -c https://www.vmware.com/go/getplayer-linux
+	chmod u+x getplayer-linux
+	sudo ./getplayer-linux
+else
+	printf "\nSkipped.\n"
+fi
 
-read -p "Press <ENTER> to install foobar2000"
-cd ~/Install-Sets/Programs/foobar2000
-wine foobar2000_v1.4.exe
+read -n1 -rsp "Preass <c> to install AnyDesk or <any other key> to skip" result
+
+if [ $result = 'c' ]; then
+	printf "\nInstalling...\n"
+	cd ~/Downloads
+	wget https://download.anydesk.com/linux/anydesk_4.0.0-1_amd64.deb
+	sudo qapt-deb-installer anydesk_4.0.0-1_amd64.deb
+else
+	printf "\nSkipped.\n"
+fi
+
+read -n1 -rsp "Preass <c> to install Viber or <any other key> to skip" result
+
+if [ $result = 'c' ]; then
+	printf "\nInstalling...\n"
+	cd ~/Downloads
+	wget -O ./viber.deb http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+	dpkg-deb -x viber.deb viber
+	dpkg-deb --control viber.deb viber/DEBIAN
+	sed -i 's/libcurl3/libcurl4/' viber/DEBIAN/control 
+	dpkg -b viber vibernew.deb
+	sudo dpkg -i vibernew.deb
+else
+	printf "\nSkipped.\n"
+fi
+
+read -n1 -rsp "Preass <c> to install foobar2000 or <any other key> to skip" result
+
+if [ $result = 'c' ]; then
+	printf "\nInstalling...\n"
+	cd ~/Install-Sets/Programs/foobar2000
+	wine foobar2000_v1.4.exe
+else
+	printf "\nSkipped.\n"
+fi

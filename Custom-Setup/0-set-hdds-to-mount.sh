db5d664036c48d628243fbf1ae28535917fdde11
Ivan Ivanov (Main PC) <ivan.ivanov.cigsso@gmail.com>
2018-11-05 22:22:25 +0200

    Updated 0-set-hdds-to-mount.sh

--- Custom-Setup/0-set-hdds-to-mount.sh
+++ Custom-Setup/0-set-hdds-to-mount.sh
@@ -10,13 +10,13 @@ mkdir /home/ivan/Movies/Anime
 mkdir /home/ivan/Movies/Movies
 
 sudo su -c "echo '# Mount /dev/sdc7 on /home/ivan/Virtual-Machines...' >> /etc/fstab"
-sudo su -c "echo 'UUID=b10e583a-0cda-4749-9fc6-d78276b18d02 /home/ivan/Virtual-Machines	ext4	defaults	0	2' >> /etc/fstab"
+sudo su -c "echo 'UUID=fee0d053-a515-4177-b379-59a6bda9fc0c /home/ivan/Virtual-Machines	ext4	defaults	0	2' >> /etc/fstab"
 
 sudo su -c "echo '# Mount /dev/sdd1 on /home/ivan/Documents...' >> /etc/fstab"
 sudo su -c "echo 'UUID=1248C54C3475BE20                     /home/ivan/Documents		ntfs	defaults	0	2' >> /etc/fstab"
 
-sudo su -c "echo '# Mount /dev/sde4 on /home/ivan/Virtual-Machines-2...' >> /etc/fstab"
-sudo su -c "echo 'UUID=fee0d053-a515-4177-b379-59a6bda9fc0c /home/ivan/Virtual-Machines-2	ext4	defaults	0	2' >> /etc/fstab"
+#sudo su -c "echo '# Mount /dev/sde4 on /home/ivan/Virtual-Machines-2...' >> /etc/fstab"
+#sudo su -c "echo 'UUID=fee0d053-a515-4177-b379-59a6bda9fc0c /home/ivan/Virtual-Machines-2	ext4	defaults	0	2' >> /etc/fstab"
 
 sudo su -c "echo '# Mount /dev/sdf1 on /home/ivan/Storage...' >> /etc/fstab"
 sudo su -c "echo 'UUID=AC0209D60209A684                     /home/ivan/Storage		ntfs	defaults	0	2' >> /etc/fstab"
@@ -37,4 +37,4 @@ sudo su -c "echo '# Mount /dev/sdh4 on /home/ivan/Movies/Movies...' >> /etc/fsta
 sudo su -c "echo 'UUID=1016F1F416F1DB24                     /home/ivan/Movies/Movies	ntfs	defaults	0	2' >> /etc/fstab"
 
 sudo su -c "chown --recursive ivan:ivan /home/ivan/Virtual-Machines"
-sudo su -c "chown --recursive ivan:ivan /home/ivan/Virtual-Machines-2"
+#sudo su -c "chown --recursive ivan:ivan /home/ivan/Virtual-Machines-2"

b099da57901bc0f34e0b5a40afd2e471bf7a3fd9
Ivan Ivanov (Main PC) <ivan.ivanov.cigsso@gmail.com>
2018-11-10 08:34:56 +0200

    Updated 3-update-grub.sh to a state of beautygit status !

--- Custom-Setup/3-update-grub.sh
+++ Custom-Setup/3-update-grub.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo su -c "cat grub.txt > /etc/default/grub"
+echo "*************************************** !!! EXTREMELY IMPORTANT!!! ****************************************"
+echo "*** Make sure \"Intel Virtualization Technology\" is enabled in Advanced -> CPU Configuration *************"
+echo "*** Make sure \"Intel VT for Directed I/O (VT-d)\" is enabled in Advanced -> System Agent Configuration ***"
+echo "***********************************************************************************************************"
+
+sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="intel_iommu=on"/' /etc/default/grub
 sudo su -c "update-grub2"
